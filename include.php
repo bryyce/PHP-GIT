82003f58b73cb15ed2da98e51e8b16b774e3838d
bryyce <bric.robin@gmail.com>
2012-04-17 15:45:53 +0200

    Déplacement des fichiers core vers le dossier ORM Changment archi fichier

--- include.php
+++ include.php
@@ -1,6 +1,25 @@
 <?php
-function __autoload($class_name) {
-    require_once dirname(__FILE__).'/'. $class_name . 'class.php';
-
-}
+    function __autoload($class_name)
+    {
+        echo $class_name.'<br>';
+        //exc
+        //class directories
+        $directorys = array(
+            './ORM',
+            './Model',
+            './',
+        );
+       
+        //for each directory
+        foreach($directorys as $directory)
+        {
+            //see if the file exsists
+            if(file_exists($directory.$class_name . '.class'))
+            {
+                require_once($directory.$class_name . '.class');
+                //only require the class once, so quit after to save effort (if you got more, then name them something else
+                return;
+            }           
+        }
+    }
 ?>
--- index.php
+++ index.php
@@ -1,8 +1,4 @@
 <?php
-require_once 'class/Config.class.php';
-require_once 'class/Enregistrement.class.php';
-require_once 'class/MyEnregistrement.class.php';
-require_once 'class/Membre.class.php';
 
 print_r(Membre::All());
 print("------------<br>");
