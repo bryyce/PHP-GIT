55364901bc135dc01e27615f3193f50fea4ff893
bryyce <bric.robin@gmail.com>
2012-05-02 21:55:19 +0200

    Ajout de la classe collection pour améliorer la gestion des tableaux
    Chagnement de la gestion du nom de table et de classe pour myenregistrement

--- index.php
+++ index.php
@@ -2,6 +2,22 @@
 //print_r(Model\Membre::All());
 print("------------<br>");
 $user = Model\User::first();
-print_r($user->profiles);
+print_r($user->profiles[0]->users[0]);
+echo '<br>';
+$c = new Collection(array(1,2,3,4));
+echo $c->next();
+echo '<br>';
+echo $c->next();
+echo '<br>';
+echo $c->next();
+echo '<br>';
+echo $c->next();
+echo '<br>';
+echo $c->next();
+echo '<br>';
+
+echo $c->remove(2);
+echo '<br>';
+print_r($c);
 echo round(memory_get_usage()/(1024),2)." Ko";
 ?>
