a65ae1e0c929ad71c1d0764035571682b04923a2
bryyce <bric.robin@gmail.com>
2012-05-08 22:39:51 +0200

    gestion foreach pour les collections -> OK
    Implémentation d'un manager de requete sql -> bien avance
    	- voir gestion des différent operateur de recherche
    	- voir la gestion de order
    	-  voir l'intégration directe du manager au recorder

--- index.php
+++ index.php
@@ -3,27 +3,14 @@
 print("------------<br>");
 $user = Model\User::all();
 print_r($user);
-echo "<br>";
+echo '<br><br>';
 $user = Model\User::first();
 print_r($user->profiles[0]->users[0]);
 $user->id = 0;
 $user->save();
 print_r($user);
-echo '<br>';
-$c = new Collection(array(1,2,3,4));
-echo $c->next();
-echo '<br>';
-echo $c->next();
-echo '<br>';
-echo $c->next();
-echo '<br>';
-echo $c->next();
-echo '<br>';
-echo $c->next();
-echo '<br>';
-
-echo $c->remove(2);
-echo '<br>';
-print_r($c);
+echo '<br><br>';
+print_r(Model\User::last());
+echo '<br><br>';
 echo round(memory_get_usage()/(1024),2)." Ko";
 ?>
