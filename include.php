94db0c67df1eda0911c546a0fcb45a7f647343b0
bryyce <bric.robin@gmail.com>
2014-11-30 21:10:51 +0100

    ajout de la gestion d'un routerz

--- include.php
+++ include.php
@@ -9,13 +9,44 @@
  * @param string $class_name Nom de la classe a charger
  * @return void 
  */
+error_reporting(E_ALL);
+
+/**
+ * 	Handler d'exception non attrapée
+ * 
+ * @param object $exception  L'Exception non attrappée
+ */
+function exceptionHandler($exception) {
+	echo "<pre>\n";
+	echo $exception->getMessage() . "\n";
+	echo $exception->getTraceAsString() . "\n";
+	echo "</pre>\n";
+}
+function getBasePath() {
+	return '/var/www/PHP-GIT/';
+}
+set_exception_handler('exceptionHandler');
+
+/**
+ * 	Présent le var_dump d'une var 
+ * 
+ * @param mixed $var la variable à afficher 
+ */
+function dump($var) {
+	echo "<pre>\n";
+	var_dump($var);
+	echo "</pre>\n";
+}
+
 function __autoload($class_name) {
 	//class directories
-	require_once($class_name . '.class');
+	require_once(getBasePath(). str_replace('\\', '/', $class_name) . '.class');
 	if (strpos($class_name, 'Model\\') === 0)
 		$class_name::loadRelations();
 	//only require the class once, so quit after to save effort (if you got more, then name them something else
 	return;
 }
-require_once 'Tool/Rooter.class';
+require_once getBasePath().'Tool/Router.class';
+require_once getBasePath().'Tool/HttpRequest.class';
+require_once getBasePath().'Config/routes.config';
 ?>
--- index.php
+++ index.php
@@ -2,17 +2,21 @@
 /**
  * index.php file
  * @filesource ./index.php
- * @package		\ 
+ * @package		\
  */
-//print_r(Model\Membre::All());
-echo Router::getCurrentRoot($_SERVER[REQUEST_URI]);
+
+var_dump( \Tool\Router::get()->getCurrentRoute(\Tool\HTTPRequest::requestURI(),\Tool\HTTPRequest::method()));
 print("------------<br>");
 $user = Model\User::all();
 print_r($user);
 echo '<br><br>';
 $user = Model\User::first();
-print_r($user->profiles[0]->users[0]);
-echo '<br><br>';
+print_r($user);
+if ($user->profiles->count() > 0){
+	print_r($user->profiles[0]->users[0]);
+	echo '<br><br>';
+
+}
 $user->login = 'abc';
 $user->save();
 print_r($user);
@@ -21,3 +25,4 @@ $user = new Model\User(1);
 print_r($user);
 echo '<br><br>';
 echo round(memory_get_usage()/(1024),2)." Ko";
+echo 'ici'; die;
