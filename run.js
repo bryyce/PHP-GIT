a859d25f9fcd2d6e0e8f08abc6cf99295807e3ff
bryyce <bric.robin@gmail.com>
2015-02-09 14:37:19 +0100

    Correction

--- run.js
+++ run.js
@@ -1,4 +1,4 @@
-var NB_RUNNER = 50;
+var NB_RUNNER = 1;
 var MAP_OPTIONS;
 
 var Run = function () {
@@ -11,20 +11,30 @@ var Run = function () {
 Run.prototype.launch = function() {
   this.map.setTilt(45)
   for (i = 0; i < NB_RUNNER; i++) {
-    this.runners[i] = new Runner("test " + i, 49.2534, 4.033, new Date(), '#'+Math.floor(Math.random()*16777215).toString(16));
+    this.runners[i] = new Runner("test " + i, 49.234903761261, 4.0691892026346, new Date(), '#'+Math.floor(Math.random()*16777215).toString(16));
   }
-  var that = this;
-  setInterval(function () { that.moveMarker() }, 10)
+  this.getPoints();
 };
 
-Run.prototype.moveMarker = function () {
-  for (i = 0; i < NB_RUNNER; i++) {
+Run.prototype.getPoints = function () {
+  /*for (i = 0; i < NB_RUNNER; i++) {
     delta_lat = ((Math.random() - 0.5)/10000)
     delta_lng = ((Math.random() - 0.5)/10000)
     this.runners[i].addPoint(this.runners[i].currentLat + delta_lat, this.runners[i].currentLng + delta_lng, new Date());
-  }
-  // if (!map.getBounds().contains(marker.getPosition()))
-  //  map.setCenter(marker.getPosition());
+  }*/
+  var that = this;
+  $.getJSON("./points/", function(points) {
+    for (i = 0; i < NB_RUNNER; i++) {
+		delta_lat =0; ((Math.random() - 0.5)/1000)
+		delta_lng =0; ((Math.random() - 0.5)/1000)
+		for(j = 0; j < points.length; j++) {
+			  run.runners[i].addPoint(points[j].lat*1 + delta_lat, points[j].lng*1 + delta_lng, points[j].date);
+		}
+	}
+	//setTimeout(run.getPoints, 100);
+  });
+ /* if (!map.getBounds().contains(marker.getPosition()))
+    map.setCenter(marker.getPosition());*/
 }
 
 var run;
@@ -32,7 +42,7 @@ function initialize() {
   MAP_OPTIONS = {
     zoom: 18,
     mapTypeId: google.maps.MapTypeId.HYBRID,
-    center: {lat:49.2534, lng: 4.033}
+    center: {lat: 49.234903761261, lng: 4.0691892026346}
   };
   (run = new Run()).launch();
 }
--- runner.js
+++ runner.js
@@ -5,6 +5,7 @@ var Runner = function (_name, _lat, _lng, _date, _color) {
   this.currentLat = _lat;
   this.currentLng = _lng;
   this.time = _date;
+  this.date = _date;
   this.marker = undefined;
   this.line = undefined;
   this.infowindow = undefined;
@@ -23,10 +24,10 @@ Runner.prototype.init = function() {
   this.infowindow = new google.maps.InfoWindow({content: this.toString()});
 
   google.maps.event.addListener(this.marker, 'click', function() {
-    if (openedWindow != undefined) {
-      openedWindow.close();
+    if (run.openedWindow != undefined) {
+      run.openedWindow.close();
     }
-    openedWindow = this.get('runner').infowindow;
+    run.openedWindow = this.get('runner').infowindow;
     this.get('runner').infowindow.open(this.get('map'), this);
   });
 
@@ -47,7 +48,9 @@ Runner.prototype.addPoint = function(lat, lng, time) {
   this.points.push({lat: lat, lng: lng});
   this.currentLat = lat;
   this.currentLng = lng;
-  this.time = time;
+  console.log(time)
+  this.time = new Date(time).getTime();
+  this.date = time;
   this.updatePosition();
 };
 
@@ -66,5 +69,5 @@ Runner.prototype.toString = function() {
   return "<h3>Coureur " + this.name + "</h3>" +
         "<div><b>Latitude : </b>" + this.currentLat + "</div>" +
         "<div><b>Longitude : </b>" + this.currentLng + "</div>" +
-        "<div><b>Dernier point : </b>" + (pad + this.time.getDay()).slice(-pad.length)  + "/" + (pad + this.time.getMonth()).slice(-pad.length)  + "/" + this.time.getFullYear()  + " "  + this.time.toLocaleTimeString() + "</div>";
+        "<div><b>Dernier point : </b>" + this.date + "</div>";
 };
